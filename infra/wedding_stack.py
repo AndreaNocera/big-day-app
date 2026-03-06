a3410bb04f5d6ecc33211f546ab612222d70edd6
Andrea Nocera <andrea.nocera@techedgegroup.com>
2026-03-06 13:08:03 +0100

    update infra

--- infra/wedding_stack.py
+++ infra/wedding_stack.py
@@ -6,7 +6,8 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_iam as iam,
     CfnOutput,
-    RemovalPolicy
+    RemovalPolicy,
+    aws_s3_deployment as s3_deploy
 )
 from constructs import Construct
 
@@ -20,31 +21,31 @@ class WeddingStack(Stack):
             partition_key=dynamodb.Attribute(name="PK", type=dynamodb.AttributeType.STRING),
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
             time_to_live_attribute="expiresAt",
-            removal_policy=RemovalPolicy.RETAIN
+            removal_policy=RemovalPolicy.DESTROY
         )
 
         rsvp_table = dynamodb.Table(self, "WeddingRSVP",
             table_name="WeddingRSVP",
             partition_key=dynamodb.Attribute(name="PK", type=dynamodb.AttributeType.STRING),
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
-            removal_policy=RemovalPolicy.RETAIN
+            removal_policy=RemovalPolicy.DESTROY
         )
         
         photos_table = dynamodb.Table(self, "WeddingPhotos",
             table_name="WeddingPhotos",
             partition_key=dynamodb.Attribute(name="PK", type=dynamodb.AttributeType.STRING),
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
-            removal_policy=RemovalPolicy.RETAIN
+            removal_policy=RemovalPolicy.DESTROY
         )
 
         photos_bucket = s3.Bucket(self, "WeddingPhotosBucket",
-            bucket_name="wedding-photos-prod", # Note: must be globally unique in real prod
+            bucket_name="wedding-photos-prod-nocera", # Changed to be more unique
             cors=[s3.CorsRule(
                 allowed_methods=[s3.HttpMethods.PUT, s3.HttpMethods.GET],
                 allowed_origins=["*"],
                 allowed_headers=["*"]
             )],
-            removal_policy=RemovalPolicy.RETAIN
+            removal_policy=RemovalPolicy.DESTROY
         )
 
         shared_env = {
@@ -61,7 +62,7 @@ class WeddingStack(Stack):
         
         # We need a layer for the shared code
         shared_layer = _lambda.LayerVersion(self, "SharedCodeLayer",
-            code=_lambda.Code.from_asset("lambda/shared"),
+            code=_lambda.Code.from_asset("../lambda/shared"),
             compatible_runtimes=[_lambda.Runtime.PYTHON_3_12],
             description="Shared helpers and AWS clients"
         )
@@ -72,31 +73,31 @@ class WeddingStack(Stack):
         # Let's map handlers
         send_invites = _lambda.Function(self, "SendInvites",
             handler="handler.handler",
-            code=_lambda.Code.from_asset("lambda/send_invites"),
+            code=_lambda.Code.from_asset("../lambda/send_invites"),
             **lambda_kwargs
         )
 
         verify_magic_link = _lambda.Function(self, "VerifyMagicLink",
             handler="handler.handler",
-            code=_lambda.Code.from_asset("lambda/verify_magic_link"),
+            code=_lambda.Code.from_asset("../lambda/verify_magic_link"),
             **lambda_kwargs
         )
         
         rsvp_handler = _lambda.Function(self, "RSVPHandler",
             handler="handler.handler",
-            code=_lambda.Code.from_asset("lambda/rsvp_handler"),
+            code=_lambda.Code.from_asset("../lambda/rsvp_handler"),
             **lambda_kwargs
         )
         
         survey_handler = _lambda.Function(self, "SurveyHandler",
             handler="handler.handler",
-            code=_lambda.Code.from_asset("lambda/survey_handler"),
+            code=_lambda.Code.from_asset("../lambda/survey_handler"),
             **lambda_kwargs
         )
         
         get_upload_url = _lambda.Function(self, "GetUploadUrl",
             handler="handler.handler",
-            code=_lambda.Code.from_asset("lambda/get_upload_url"),
+            code=_lambda.Code.from_asset("../lambda/get_upload_url"),
             **lambda_kwargs
         )
 
@@ -129,5 +130,26 @@ class WeddingStack(Stack):
         api.root.add_resource("rsvp").add_method("POST", apigw.LambdaIntegration(rsvp_handler))
         api.root.add_resource("survey").add_method("POST", apigw.LambdaIntegration(survey_handler))
         api.root.add_resource("upload").add_resource("url").add_method("POST", apigw.LambdaIntegration(get_upload_url))
+        
+        # Frontend Hosting
+        frontend_bucket = s3.Bucket(self, "WeddingFrontendBucket",
+            bucket_name="wedding-frontend-prod-nocera", # Explicit unique name
+            website_index_document="index.html",
+            public_read_access=True,
+            block_public_access=s3.BlockPublicAccess(
+                block_public_acls=False,
+                block_public_policy=False,
+                ignore_public_acls=False,
+                restrict_public_buckets=False
+            ),
+            removal_policy=RemovalPolicy.DESTROY, # RETAIN in prod if preferred
+            auto_delete_objects=True
+        )
+
+        s3_deploy.BucketDeployment(self, "DeployFrontend",
+            sources=[s3_deploy.Source.asset("../frontend/out")],
+            destination_bucket=frontend_bucket
+        )
 
         CfnOutput(self, "ApiUrl", value=api.url)
+        CfnOutput(self, "FrontendUrl", value=frontend_bucket.bucket_website_url)
